606a2d4ffb9e6870fb40f051e0725e107e4b59a0
DinhHongKong <83532836+dinhhongkong@users.noreply.github.com>
2024-05-04 18:44:55 +0700

    update security

--- Smartphone-E-commerce-Website/src/main/java/com/cuahangdienthoai/config/VNPayConfig.java
+++ Smartphone-E-commerce-Website/src/main/java/com/cuahangdienthoai/config/VNPayConfig.java
@@ -17,8 +17,8 @@ import java.util.*;
 public class VNPayConfig {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static String vnp_ReturnUrl = "/vnpay-payment";
-    public static String vnp_TmnCode = "N05XYEQA";
-    public static String vnp_HashSecret = "SOSIBCNHHSKDGEWVZOFTHDSZRLZOYOIN";
+    public static String vnp_TmnCode = "";
+    public static String vnp_HashSecret = "";
     public static String vnp_apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
     public static String md5(String message) {
